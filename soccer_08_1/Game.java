1f7a5faf3c4320e7a0dca9f1beceda7bcfe0e5ba
Sheldon John Van Zyl <123169408+SheldonVZyl@users.noreply.github.com>
2024-04-12 09:59:20 +0200

    Add files via upload

---
+++ soccer_08_1/Game.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package soccer_08_1;
+
+import soccer_07_1.*;
+import soccer_06_2.*;
+import soccer_06_1.*;
+
+/**
+ *
+ * @author sheld
+ */
+public class Game {
+    public Team hometeam;
+    public Team awayTeam;
+    public Goal[] goals;
+    soccer_06_2.Team homeTeam;
+}
