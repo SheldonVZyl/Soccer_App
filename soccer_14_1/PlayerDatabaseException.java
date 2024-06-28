2c5776f346fbe8254777b79675ece782568c441a
Sheldon John Van Zyl <123169408+SheldonVZyl@users.noreply.github.com>
2024-06-28 13:11:39 +0200

    Add files via upload

---
+++ soccer_14_1/PlayerDatabaseException.java
@@ -0,0 +1,21 @@
+package soccer_14_1;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author sheld
+ */
+public class PlayerDatabaseException extends Exception {
+
+    public PlayerDatabaseException() {
+    }
+
+    public PlayerDatabaseException(String message) {
+        super(message);
+    }
+
+}
