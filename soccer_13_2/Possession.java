f152180204b635fdc29df3cb5a184891e72d8f81
Sheldon John Van Zyl <123169408+SheldonVZyl@users.noreply.github.com>
2024-06-28 13:10:48 +0200

    Add files via upload

---
+++ soccer_13_2/Possession.java
@@ -0,0 +1,20 @@
+package soccer_13_2;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author sheld
+ */
+public class Possession extends GameEvent {
+    
+    public String toString(){
+        return "Possession";
+    }
+    
+}
